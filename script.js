82960b5764f577e73110b30e65ea3a35d2ea29f1
Davhho-dev <Davhho@utexas.edu>
2022-10-18 20:27:51 -0500

    Provide padding to footer

--- script.js
+++ script.js
@@ -2,9 +2,13 @@ const button = document.querySelector("button");
 const firstName = document.getElementById("first_name");
 const form = document.getElementById("form");
 
-// button.addEventListener("click", (e) => {
-//     button.setAttribute("style", "background-color: #35402b");
-//     setTimeout(function() {
-//         button.setAttribute("style", "background-color: #596C48");
-//     }, 50);
-// });
+form.addEventListener("submit", (e) => {
+    console.log(firstName.value);
+});
+
+button.addEventListener("click", (e) => {
+    button.setAttribute("style", "background-color: #35402b");
+    setTimeout(function() {
+        button.setAttribute("style", "background-color: #596C48");
+    }, 50);
+});
